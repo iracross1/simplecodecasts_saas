e18955584b5e1ebce55ada7a90610d49c291bf8a
iracross1 <cross.i@hotmail.com>
2018-09-17 12:33:20 -0400

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,21 @@
 class ProfilesController < ApplicationController
+  def new
+  # form where user can fill out their own profile. 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end
+  def create
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      redirect_to user_path( params [:user_id])
+      flash [:success] = "Profile Update!"
+    else
+      render action :new
+  end
   
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
 end
