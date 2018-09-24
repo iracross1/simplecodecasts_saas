13f78fc300e053c52af684a6df958a264fbc9b7e
iracross1 <cross.i@hotmail.com>
2018-09-24 16:18:29 -0400

    Made corrections to index and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @user = User.all
   end
-  
+   
   def show
     @user = User.find( params[:id] )
   end
